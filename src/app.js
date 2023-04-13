9f2ce3e498c10eabfe0dd37483447f99c00544fb
Russs11 <russs.khalilov@gmail.com>
2023-04-13 16:42:43 +0300

    math_numbers_13.04.2023

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
 'use strict'
 
-let weatherMap = new Map([
-	['London', 10],
-	['Moscow', 7],
-	['Paris', 14],
+// let weatherMap = new Map([
+// 	['London', 10],
+// 	['Moscow', 7],
+// 	['Paris', 14],
 
-]);
-// let weatherMap2 = new Map()
-// for (const [key, value] of weatherMap) {
-// 	weatherMap2.set(value, key)
-// }
-weatherMap =  new Map([...weatherMap].map(el => el.reverse()))
-console.log(weatherMap);
+// ]);
+// // let weatherMap2 = new Map()
+// // for (const [key, value] of weatherMap) {
+// // 	weatherMap2.set(value, key)
+// // }
+// weatherMap =  new Map([...weatherMap].map(el => el.reverse()))
+// console.log(weatherMap);
+
+function randomNumber(min, max) {
+	return Math.floor(Math.random() * (max - min) + min);
+}
+
+console.log(randomNumber(1, 10));
